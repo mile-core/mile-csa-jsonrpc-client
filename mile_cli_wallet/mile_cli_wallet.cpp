d95f09b612ad94a2e6812db2251751d08ac04134
lotus-mile <develop@mile.global>
2018-11-22 20:14:50 +0300

    cli wallet helper updated

--- mile_cli_wallet/mile_cli_wallet.cpp
+++ mile_cli_wallet/mile_cli_wallet.cpp
@@ -147,7 +147,10 @@ static bool parse_cmdline(int ac, char *av[]) {
         "\tget-wallet-state --params '{\"public-key\": \"...bHqSm8WTuY3gB9UXD...\"}' \n"\
         "\tget-wallet-transactions --params '{\"public-key\": \"...bHqSm8WTuY3gB9UXD...\"}' \n"\
         "\tsend-transfer --params '{\"private-key\": \"...bHqSm8WTuY3gB9UXD...\", \"to\":\"...pXg1MF4qxZTEsL..\", \"amount\": \"100\", \"asset-code\":0, \"description\":\"send my money back!\"}' \n"\
-         "\tsend-emission --params '{\"private-key\": \"...bHqSm8WTuY3gB9UXD...\", \"asset-code\":0}' \n"\
+        "\tsend-emission --params '{\"private-key\": \"...bHqSm8WTuY3gB9UXD...\", \"asset-code\":0}'\n"\
+        "\tregister-node --params '{\"private-key\": \"...bHqSm8WTuY3gB9UXD....\", \"amount\": 10000.00, \"address\": \"xxx.yyy.zzz.www\"}'\n"\
+        "\tunregister-node --params '{\"private-key\": \"...bHqSm8WTuY3gB9UXD....\"}'\n"\
+        "\tvote-for-rate -p '{\"private-key\": \"...bHqSm8WTuY3gB9UXD....\", \"amount\": 1.21}'\n"\
         "";
 
         po::variables_map vm;
